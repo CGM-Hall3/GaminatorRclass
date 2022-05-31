94913739332fda9af18ffeaee0c3a2844e4e145b
dataRufus <rufussuccess96@yahoo.com>
2022-05-31 11:32:56 +0100

    Reading files

--- read_file.R
+++ read_file.R
@@ -2,6 +2,15 @@ install.packages('readr')
 
 library('readr')
 
-path_data <- "/c/Users/USER/Downloads/Iris.csv"
+path_data <- "C:/Users/USER/Downloads/Iris.csv"
+#"C:\Users\USER\Downloads\Iris.csv"
 df_table <- read_csv(path_data)
-print(df_table)
+#print(df_table)
+View(df_table)
+class(df_table)
+head(df_table)
+for (i in colnames(df_table)){
+  print(i)
+}
+ncol(df_table)
+nrow(df_table)
