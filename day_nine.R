d980098f7b3292f250acac7149c1fb44cc582939
CGM-Hall3 <rufussuccess96@yahoo.com>
2022-06-07 11:14:31 +0100

    Continued today

--- day_nine.R
+++ day_nine.R
@@ -2,7 +2,8 @@ library("readr")
 #install.packages("janitor")
 library("janitor")
 library("dplyr")
-
+#install.packages("lubridate")
+library("lubridate")
 
 path <- "C:/Users/USER/Desktop/Data 1/Assignment/finnstats/FinData.csv"
 df <- read_csv(path)
@@ -26,8 +27,6 @@ formatt <- clean_df %>% tabyl(i) %>%
 print(formatt)
 }
 
-clean_df %>% tabyl(employee_status, full_time) %>% adorn_totals()
-
 clean_df %>% tabyl(employee_status, full_time) %>%
   adorn_totals(where = c("row", "col"))
 
@@ -42,7 +41,6 @@ clean_new %>% get_dupes(first_name)
 dat <- clean_new %>% get_dupes(first_name, certification_9)
 View(dat)
 
-
 b <- clean_new$hire_date
 print(b)
 
@@ -50,12 +48,28 @@ filtered_one <- clean_new %>% filter(!grepl('/', hire_date))
 View(filtered_one)
 filtered_two <- clean_new %>% filter(grepl('/', hire_date))
 View(filtered_two)
+glimpse(filtered_two)
+num_date <- as.numeric(filtered_one$hire_date)
+clean_date <- excel_numeric_to_date(num_date)
+clean_date
 
-f1_date <- c()
-for (i in filtered_one$hire_date){
-clean_date <- excel_numeric_to_date(as.numeric(i))
-#print(clean_date)
-f1_date <- c(f1_date, clean_date)
-}
-print(f1_date)
-#clean_date <- excel_numeric_to_date(i)
+clean_one <- filtered_one %>% mutate(hire_date = clean_date)
+View(clean_one)
+
+new_dt <- mdy(filtered_two$hire_date)
+clean_two <- filtered_two %>% 
+  mutate(hire_date = new_dt)
+View(clean_two)
+
+new_df <- rbind(clean_one, clean_two)
+View(new_df)
+
+# for (i in colnames(new_df)){
+# k <- sum(is.na(new_df[i]))
+# p <- paste(i, "=>", k, sep=" ")
+# print(p)
+# }
+
+# new_df$subject[is.na(new_df$subject)] <- mode(new_df$subject)
+# new_df$certification_9[is.na(new_df$certification_9)] <- mode(new_df$certification_9)
+# new_df$certification_10[is.na(new_df$certification_10)] <- mode(new_df$certification_10)
